7bfd34608390854c61b16d112cba7137ca96d07a
lanmonster <kyle.lanmon@gmail.com>
2024-02-21 10:28:09 -0600

    editors: Delete unused handler

--- editors/vscode/server/src/server.ts
+++ editors/vscode/server/src/server.ts
@@ -221,12 +221,6 @@ connection.onHover(async (request: HoverParams) => {
 
 connection.onDidChangeConfiguration(change => documentsManager.onDidChangeConfiguration(change));
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-connection.onDidChangeWatchedFiles(_change => {
-    // Monitored files have change in VSCode
-    connection.console.log("We received an file change event");
-});
-
 // This handler provides the initial list of the completion items.
 connection.onCompletion(async (request: TextDocumentPositionParams): Promise<CompletionItem[]> => {
     return await logDuration(`onCompletion ${clickableFilePosition(request)}`, async () => {
@@ -339,19 +333,6 @@ connection.onDocumentRangeFormatting(async params => {
         }
         return [];
     });
-});
-
-// This handler resolves additional information for the item selected in
-// the completion list.
-connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
-    if (item.data === 1) {
-        item.detail = "TypeScript details";
-        item.documentation = "TypeScript documentation";
-    } else if (item.data === 2) {
-        item.detail = "JavaScript details";
-        item.documentation = "JavaScript documentation";
-    }
-    return item;
 });
 
 connection.languages.inlayHint.on(async (params: InlayHintParams) => {
